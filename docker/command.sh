598c2d81625cf9c2303309ae3232d56e79d10c34
Akram Khan <akrammiru@gmail.com>
2021-12-07 17:14:52 +0600

    pushed docker to ecr

--- docker/command.sh
+++ docker/command.sh
@@ -1,3 +1,4 @@
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/r3g6z8h3
 docker build -t kafka-java:0.0.1 .
-docker tag kafka-java:0.0.1 825783678861.dkr.ecr.ap-southeast-1.amazonaws.com/kafka-java:0.0.1
-docker push 825783678861.dkr.ecr.ap-southeast-1.amazonaws.com/kafka-java:0.0.1
+docker tag kafka-java:0.0.1 public.ecr.aws/r3g6z8h3/kafka-java:0.0.1
+docker push public.ecr.aws/r3g6z8h3/kafka-java:0.0.1
